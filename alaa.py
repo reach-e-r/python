9d602740dd26c13d9dac71d21ee768413bb19fa8
reach-e-r <kimutaii004@gmail.com>
2023-10-25 20:33:39 +0300

    methods

---
+++ alaa.py
@@ -0,0 +1,6 @@
+import maximum
+beef = [56,678,900,2543,23,12,87,8]
+print(maximum.find_max(beef))
+
+
+
---
+++ nloop.py
@@ -0,0 +1,8 @@
+numbers = [5, 2, 5, 2, 2]
+for i in numbers:
+    output = ""
+    for y in range(i):
+        output += "x"
+    print(output)
+
+
